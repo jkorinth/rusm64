6e2dc01e9720edfb5a1aabccecf0193ed8d778a9
Jens Korinth <jens.korinth@tuta.io>
2025-05-29 22:30:26 +0200

    refactor: Move and improve Errors

---
+++ src/error.rs
@@ -0,0 +1,37 @@
+use derive_more::From;
+
+use super::assembler::{AssembleError, AssembleErrorList};
+use super::parser::grammar::ParseError;
+
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
+pub enum Error {
+    #[error("Parser error: {0}")]
+    Parse(#[from] ParseError),
+
+    #[error("Assembler error: {0}")]
+    Assembler(#[from] AssembleError),
+
+    #[error("multiple errors:\n{0}")]
+    MultipleErrors(#[from] ErrorList),
+}
+
+#[derive(Clone, Debug, Eq, From, PartialEq, thiserror::Error)]
+pub struct ErrorList(Vec<Error>);
+
+impl std::fmt::Display for ErrorList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("f")
+    }
+}
+
+impl From<AssembleErrorList> for ErrorList {
+    fn from(value: AssembleErrorList) -> Self {
+        Self(
+        value
+            .iter()
+            .map(|(_, e)| Error::Assembler(e.clone()))
+            .collect::<Vec<_>>())
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
