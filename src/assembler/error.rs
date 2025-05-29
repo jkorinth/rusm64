a395cc34760bbdabc5bfcb41fd7bf320291bdc97
Jens Korinth <jens.korinth@tuta.io>
2025-05-29 22:30:26 +0200

    refactor: Move and improve Errors

---
+++ src/assembler/error.rs
@@ -0,0 +1,55 @@
+use crate::ast::{Expr, Op};
+use std::num::ParseIntError;
+
+use super::SourceLocation;
+
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
+pub enum AssembleError {
+    #[error("not implemented yet")]
+    Problem,
+
+    #[error("invalid numeric literal found: {0}")]
+    InvalidLiteral(ParseIntError),
+
+    #[error("cannot evaluate expression: {0}")]
+    CannotEvaluateExpr(Expr),
+
+    #[error("cannot evaluate literal: {0}")]
+    CannotEvaluateLiteral(String),
+
+    #[error("duplicate label found: {0}")]
+    DuplicateLabel(String),
+
+    #[error("invalid instruction found: {0}")]
+    InvalidInstruction(Op),
+
+    #[error("multiple errors:\n{0}")]
+    MultipleErrors(#[from] AssembleErrorList),
+}
+
+impl From<Vec<(SourceLocation, AssembleError)>> for AssembleError {
+    fn from(value: Vec<(SourceLocation, AssembleError)>) -> Self {
+        Self::MultipleErrors(value.into())
+    }
+}
+
+#[derive(Clone, Debug, derive_more::Deref, Eq, PartialEq, thiserror::Error)]
+pub struct AssembleErrorList(Vec<(SourceLocation, AssembleError)>);
+
+impl From<Vec<(SourceLocation, AssembleError)>> for AssembleErrorList {
+    fn from(value: Vec<(SourceLocation, AssembleError)>) -> Self {
+        Self(value)
+    }
+}
+
+impl std::fmt::Display for AssembleErrorList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("f")
+    }
+}
+
+impl From<ParseIntError> for AssembleError {
+    fn from(value: ParseIntError) -> Self {
+        Self::InvalidLiteral(value)
+    }
+}
